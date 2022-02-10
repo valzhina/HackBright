d2b1e418cc612a7c7d62f75dafff71fb0b52491e
Olya <valzhinapiskun@gmail.com>
2022-02-10 05:47:30 +0000

    Modified Arithmetic

--- arithmetic.py
+++ arithmetic.py
@@ -2,35 +2,49 @@
 
 
 def add(num1, num2):
-    """Return the sum of num1 and num2."""
+    """Return the sum of the two input integers."""
 
     return num1 + num2
 
 
 def subtract(num1, num2):
-    """Return the value of num1 minus num2."""
+    """Return the second number subtracted from the first."""
+
     return num1 - num2
 
+
 def multiply(num1, num2):
-    """Multiply the num1 by num2 and return the result."""
+    """Multiply the two inputs together."""
+
     return num1 * num2
 
+
 def divide(num1, num2):
-    """Divide the num1 by num2, returning a floating point."""
+    """Divide the first input by the second, returning a floating point."""
+
     return num1 / num2
 
+
 def square(num1):
-    """Return the square of num1."""
+    """Return the square of the input."""
+
     return num1 * num1
 
+
 def cube(num1):
-    """Return the cube of num1."""
+    """Return the cube of the input."""
+
     return num1 * num1 * num1
 
+
 def power(num1, num2):
-    """Raise num1 to the power of num2 and return the value."""
-    return num1 ** num2
+    """Raise num1 to the power of num and return the value."""
+
+    return num1 ** num2  # ** = exponent operator
+
 
 def mod(num1, num2):
-    """Return the remainder of num1 / num2."""
+    """Return the remainder of num / num2."""
+
     return num1 % num2
+
