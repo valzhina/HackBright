6f86f70eb6e8cff951483f42bc3d1b3627253643
Olya <valzhinapiskun@gmail.com>
2022-03-17 05:49:53 +0000

    initial commit

---
+++ fertility_app/model.py
@@ -0,0 +1,115 @@
+"""Models for fertility app."""
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, date
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """Stores information about each user"""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True,
+                        unique=True)
+    full_name = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(100), unique=True, nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+    profile_proto_url = db.Column(db.String(100), nullable = True, default = '/static/img/TBD_default-profile-photo.png')
+    created_at = db.Column(db.DateTime)
+
+    # body_temperature = a list of Items objects --> an attribute has been made for the Temperature Class
+    # supplements = a list of Items objects --> an attribute has been made for the Supplement Class
+    # meals = a list of Items objects --> an attribute has been made for the Meal Class
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} full_name={self.full_name} email={self.email}>'
+
+
+class Temperature(db.Model):
+    """Stores all the entries of temperature that are written for each user"""
+    __tablename__ = 'body_temperature'
+
+    temperature__entry_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True,
+                        unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    temperature__entry = db.Column(db.String(10), nullable=True)
+    date_time = db.Column(db.String, nullable = False, default=datetime.today())
+
+    user = db.relationship('User', backref="body_temperature")
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} temperature__entry={self.temperature__entry} >'
+
+
+class Supplement(db.Model):
+    """Stores all the entries of Supplements that are written for each user"""
+    __tablename__ = 'supplements'
+
+    supplement_entry_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True,
+                        unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    supplement_entry = db.Column(db.String(100), nullable=True)
+    date_time = db.Column(db.String, nullable = False, default=datetime.today())
+
+    user = db.relationship('User', backref="supplements")
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} supplement_entry={self.supplement_entry} >'
+
+class Meal(db.Model):
+    """Stores all the entries of meals that are written for each user"""
+    __tablename__ = 'meals'
+
+    meal__entry_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True,
+                        unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    meal_entry = db.Column(db.String(100), nullable=True)
+    date_time = db.Column(db.String, nullable = False, default=datetime.today())
+
+    user = db.relationship('User', backref="meals")
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} meal_entry={self.meal_entry}>'
+
+# class Water(db.Model):
+#     """Stores all the entries of water that are written for each user"""
+
+# class Menstruation(db.Model):
+#     """Stores all the entries of menstruation that are written for each user"""
+
+# class Notes(db.Model):
+#     """Stores all the entries of text that are written for each user"""
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///fertility", echo=True):
+    """Connests the database to the Flaskapp"""
+
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
+    db.create_all()
---
+++ fertility_app/server.py
@@ -0,0 +1,150 @@
+"""Server for fertility app."""
+
+from flask import (Flask, render_template, request, flash, session, redirect)
+
+from model import connect_to_db, db
+# import crud
+
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+
+# A secret key is needed to use Flask sessioning features
+app.secret_key = 'this-should-be-something-unguessable'
+
+# Normally, if you refer to an undefined variable in a Jinja template,
+# Jinja silently ignores this. This makes debugging difficult, so we'll
+# set an attribute of the Jinja environment that says to make this an
+# error.
+app.jinja_env.undefined = StrictUndefined
+
+# app.jinja_env.auto_reload = True
+
+MOST_LOVED_ACTIVITY = {
+    "kate": {
+        "img": "https://unsplash.com/photos/uM18y03NkNg",
+        "name": "Kate_Hliznitsova",
+        "num_loves": 3,
+    },
+}
+
+#  Creating this first route and view functions
+@app.route("/")
+def show_index():
+    """Return homepage."""
+    if 'name' in session:
+        return redirect("/features")
+    return render_template('homepage.html')
+
+
+@app.route("/features")
+def list_features():
+    """Return page showing all the features app has to offer"""
+
+    feature_list = features.get_all()
+    return render_template("all_features.html",
+                           feature_list=feature_list)
+
+# @app.route("/get-name")
+# def get_name():
+#     session['name'] = request.args["name"]
+    
+#     return redirect('/all_features')
+
+
+@app.route("/all_features")
+def top_activities():
+    """Return page showing top activities"""
+    if 'name' in session:
+        return render_template("first-page.html", activity = MOST_LOVED_ACTIVITY)
+    return redirect("/")
+    
+
+# User Routs...
+@app.route("/register", methods=["GET"])
+def show_register():
+    """Show register form."""
+
+    return render_template("register.html")
+
+
+@app.route("/register", methods=["POST"])
+def process_register():
+    """Register user information to DB """
+
+    first_name = request.form.get('addFirstName')
+    last_name = request.form.get('addLastName')
+    email = request.form.get('addEmail')
+    password = request.form.get('addPasword')
+    confirm_password = request.form.get('addConfirmPasword')
+    # profile_proto_url = request.form.get('addPhoto')
+    # created_at = date and time
+
+    user = User.query.filter_by(email = email).first()
+
+    if user != NULL:
+        flash('An account with this email already exists')
+    
+    if password != confirm_password:
+        flash('Password does not match, please try again')
+
+    else:
+        new_user = User(first_name = first_name,
+                        last_name = last_name,
+                        email = email,
+                        password = password,
+                        # profile_proto_url
+                        created_at = datetime.today())
+    
+    # set password for new user
+    db.session.add(new_user)
+    db.session.commit()
+
+    first_time_user = User.query.filter
+        
+
+@app.route("/login", methods=["GET"])
+def show_login():
+    """Show login form."""
+
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Log user into site."""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = user.get_by_email(email) 
+    # user = User.query.filter_by(email = email).first()
+  
+    if user:
+        if pasword == user.password:
+            session['logged_in_customer_name'] = user.first_name
+            flash('Login successful')
+            return redirect('/features')
+        else:
+            flash('Incorrect password')
+            
+    else:
+        flash('Incorrect login')
+
+    return redirect('/login')
+
+
+@app.route("/logout")
+def process_logout():
+    del session['logged_in_customer_name']
+    flash("Logged out")
+    return redirect('homepage.html')   
+
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        use_reloader=True,
+        use_debugger=True,
+    )
